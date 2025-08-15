15e87ed908e71d907fa065a599d660d5bf42d575
Arefaris <arefaris6969@gmail.com>
2025-08-15 13:20:47 +0300

    home page styling

--- frontend/src/components/home/Home.tsx
+++ frontend/src/components/home/Home.tsx
@@ -1,5 +1,5 @@
 import { useRef, useState } from 'react'
-import {Route, Routes, Link, useNavigate } from 'react-router'
+import { Route, Routes, Link, useNavigate } from 'react-router'
 import useStore from '../../store/store';
 import CountrySelector from '../countrySelector/CountrySelector';
 import DateSelector from '../dateSelector/DateSelector';
@@ -9,37 +9,39 @@ import TravelTypeSelector from '../travelTypeSelector/TravelTypeSelector';
 import "./style.css"
 export default function Home() {
   const navigate = useNavigate()
-   const { plan, setPlan, travelerTypes } = useStore()
-  
-  const run = ()=> {
+  const { plan, setPlan, travelerTypes } = useStore()
+
+  const run = () => {
     console.log(plan)
   }
+
   return (
-  
-  <div className="home">
 
-        <div className="flex">
-          <h1>SmartTrip</h1>
+    <div className="home">
 
-            <div className="country-date-travel">
-              <CountrySelector /> 
-              
-              <DateSelector />
+      <div className="flex">
+        <h1 className="app-logo">SmartTrip</h1>
+        <h2 className="hero-title">Create AI-powered travel plans</h2>
 
-              <TravelTypeSelector /> 
-              <button className="plan-btn" onClick={() => {run()}}>Plan trip</button>
-            </div>
-            
-              <div className="budget-travel-cont">
-                <BudgetSelector />
-                <InterestSelector /> 
-              </div>
-          
-          </div>  
-        
-          
-          
+        <div className="country-date-travel">
+          <CountrySelector />
+
+          <DateSelector />
+
+          <TravelTypeSelector />
+          <button className="plan-btn" onClick={() => { run() }}>Plan trip</button>
         </div>
-    
+
+        <div className="budget-travel-cont">
+          <BudgetSelector />
+          <InterestSelector />
+        </div>
+
+      </div>
+
+
+
+    </div>
+
   )
 }
