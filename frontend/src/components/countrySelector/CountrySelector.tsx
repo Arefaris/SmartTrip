ebe7f538f3c673c29159e3532e95945756ac6fc8
Arefaris <arefaris6969@gmail.com>
2025-08-20 13:21:56 +0300

    added loader for for country selector

--- frontend/src/components/countrySelector/CountrySelector.tsx
+++ frontend/src/components/countrySelector/CountrySelector.tsx
@@ -4,7 +4,7 @@ import type { city_country } from '../../types';
 import useStore from '../../store/store';
 import { Autocomplete } from '@mantine/core';
 import './style.css';
-
+import { Loader } from '@mantine/core';
 
 const url = import.meta.env.VITE_BASE_URL
 
@@ -12,18 +12,19 @@ export default function CountrySelector() {
   const { plan, setPlan } = useStore()
   const [options, setOptions] = useState<string[]>([]);
   const [inputValue, setInputValue] = useState(plan.location);
- 
+  const [fetching, setFetching] = useState(false)
 
   //fetching for auto-complete
   const fetchCityCountry = useCallback(async (query: string) => {
     if (query.length < 3) {
       setOptions([]);
+      setFetching(false)
       return;
     }
 
     try {
       const result = await axios.post(`${url}/api/county-list`, { query })
-
+      
       if (result.status === 200) {
         //Transform API data into "City, Country" format strings
         const cityOptions = result.data.result.map((country: city_country) => `${country.name}, ${country.country_name}`) as string[]
@@ -38,15 +39,18 @@ export default function CountrySelector() {
         //Remove duplicates using Set
         const uniqueOptions = [...new Set(allOptions)]
         setOptions(uniqueOptions)
+        setFetching(false)
       }
 
     } catch (error) {
+      setFetching(false)
       console.log(error)
     }
   }, [])
 
   //setting location
   const handleChange = useCallback((value: string) => {
+    setFetching(true)
     setInputValue(value);
     fetchCityCountry(value);
   }, [fetchCityCountry])
@@ -61,8 +65,8 @@ export default function CountrySelector() {
     }
   }, [plan, setPlan])
 
-  return (
-    <Autocomplete
+  return (<div className="complete-loader-cont">
+     <Autocomplete
       label="Where to?"
       placeholder="Enter country or city"
       data={options}
@@ -72,5 +76,8 @@ export default function CountrySelector() {
       onOptionSubmit={handleOptionSubmit}
       className="country-selector"
     />
+    {fetching && <Loader className='inpt-loader' size="sm" color="white" />}
+  </div>
+   
   )
 }
